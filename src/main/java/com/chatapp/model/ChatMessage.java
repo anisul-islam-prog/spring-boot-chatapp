1fc3f11cd4123064670e55a23ed66889990d1333
Anisul Islam <kingfahad0802@gmail.com>
2020-06-27 05:22:41 +0600

    WebSocket Server side implementation with in memory message broker :heavy_check_mark:

---
+++ src/main/java/com/chatapp/model/ChatMessage.java
@@ -0,0 +1,75 @@
+package com.chatapp.model;
+
+public class ChatMessage {
+	private MessageType type;
+	private String content;
+	private String sender;
+
+	public enum MessageType {
+		CHAT, JOIN, LEAVE
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	@Override
+	public String toString() {
+		return "ChatMessage [type=" + type + ", content=" + content + ", sender=" + sender + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((sender == null) ? 0 : sender.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChatMessage other = (ChatMessage) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (sender == null) {
+			if (other.sender != null)
+				return false;
+		} else if (!sender.equals(other.sender))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+}
